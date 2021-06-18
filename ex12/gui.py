891f27060cff8a5f34f6fda4b251dea0c14bce81
S1rEBrum <fortus.illia1@gmail.com>
2021-06-18 21:59:23 +0300

    GUI

--- ex12/gui.py
+++ ex12/gui.py
@@ -1,16 +1,25 @@
 import tkinter as tk
-#from tkinter import messagebox
 from functools import partial
 from boggle_board_randomizer import randomize_board
-
 
 START_MSG = "Welcome to the Boggle game! Here you need to find as many words as " \
             "possible in three minutes. Are you ready to start?"
 
 
-#def start():
-#    messagebox.showinfo(title="Let`s play!", message=START_MSG)
-#    return True
+def readfile(file):
+    words = []
+    f_words = open(file)
+    for line in f_words:
+        word = line.strip()
+        if word.isalpha():
+            words.append(line.strip())
+    f_words.close()
+    return words
+
+
+def randomize():
+    return randomize_board()
+
 
 class GUI:
     CELLS_IN_ROW = 4
@@ -18,8 +27,10 @@ class GUI:
 
     def __init__(self, root):
         self._root = root
-        self.word = "WORD: "
+        self.word = ""
         self.buttons = dict()
+        self.board = randomize()
+        self.score = 0
         root.geometry("600x700")
         tk.Grid.rowconfigure(root, 0, weight=1)
         tk.Grid.rowconfigure(root, 1, weight=1)
@@ -30,16 +41,13 @@ class GUI:
         self.desk_frame.grid(row=1, column=0, sticky="snew")
         self._create_grid(self.desk_frame)
         self._create_menu(self.text_frame)
-        #if start():
-        #    self.countdown()
 
     def _create_grid(self, frame):
-        board = randomize_board()
         for i in range(self.CELLS_IN_ROW):
             tk.Grid.rowconfigure(frame, i, weight=1)
             for j in range(self.CELLS_IN_ROW):
                 tk.Grid.columnconfigure(frame, j, weight=1)
-                button = tk.Button(frame, text=board[i][j],
+                button = tk.Button(frame, text=self.board[i][j],
                                    width=6, height=3, command=partial(self.button_action, i, j),
                                    font=("Comic Sans MS", 15, "bold"), relief=tk.GROOVE,
                                    background='mint cream', activebackground="LightBlue1", fg="blue4")
@@ -58,10 +66,21 @@ class GUI:
 
     def countdown(self):
         pass
-        #count = 180
-        #self.timer['text'] = count
-        #if count > 0:
-        #    root.after(1000, count - 1, self.countdown)
+
+    def update_score(self):
+        self.score += int(len(self.word)) * 2
+        self.score_value["text"] = str(self.score)
+
+    def check_word(self):
+        words_list = readfile("boggle_dict.txt")
+        if self.word in words_list:
+            self.update_score()
+            self.word = ""
+            for (i, j) in self.buttons.keys():
+                self.buttons[(i, j)].configure(background="light cyan")
+
+    def refresh_board(self):
+        self.board = randomize()
 
     def _create_menu(self, frame):
         tk.Grid.rowconfigure(frame, 0, weight=1)
@@ -75,19 +94,25 @@ class GUI:
                              background="LightBlue1", fg="blue4", font=("Snap ITC", 20))
         self.word_label = tk.Label(frame, text=self.word, background="LightBlue1",
                                    fg="blue4", font=("Comic Sans MS", 12))
-        score = tk.Label(frame, text="SCORE:", background="LightBlue1", fg="blue4", font=("Comic Sans MS", 12))
+        score_label = tk.Label(frame, text="SCORE:", background="LightBlue1", fg="blue4", font=("Comic Sans MS", 12))
+        self.score_value = tk.Label(frame, background="LightBlue1", fg="blue4", font=("Comic Sans MS", 12))
         time_label = tk.Label(frame, background="LightBlue1", bitmap="hourglass", fg="blue4")
         check_word = tk.Button(frame, text='CHECK', background="LightBlue1",
-                               activebackground="PaleGreen1",
+                               activebackground="PaleGreen1", command=self.check_word,
                                fg="blue4", font=("Comic Sans MS", 10, "bold"))
-        #self.timer = tk.Label(frame, background="LightBlue1", font=("Franklin Gothic Medium", 10))
+        refresh = tk.Button(frame, text='REFRESH', background="LightBlue1",
+                            activebackground="PaleGreen1", command=self.refresh_board,
+                            fg="blue4", font=("Comic Sans MS", 10, "bold"))
+        # self.timer = tk.Label(frame, background="LightBlue1", font=("Franklin Gothic Medium", 10))
 
         game_name.grid(row=0, column=1)
-        score.grid(row=1, column=2)
+        score_label.grid(row=1, column=2)
+        self.score_value.grid(row=1, column=3)
         time_label.grid(row=1, column=0)
-        #self.timer.grid(row=1, column=3)
+        # self.timer.grid(row=1, column=3)
         self.word_label.grid(row=1, column=1)
-        check_word.grid(row=2, column=1)
+        check_word.grid(row=2, column=0)
+        refresh.grid(row=2, column=2)
 
 
 if __name__ == "__main__":
