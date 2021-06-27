bab9c729a0acc96af52cf95f1dbcfed1e4951214
yedidyatob <yedidyat@gmail.com>
2021-06-27 09:51:04 +0300

    finished tiud of utils

--- ex12/ex12_utils.py
+++ ex12/ex12_utils.py
@@ -1,8 +1,12 @@
+###################################
+# Files: ex12_utils_py, gui.py
+# WRITER: Yedidya_Toberman, Yedidyat, 318533197
+#            Illia_FORTUS, illia_fortus, 342851284
+# EXERCISE: intro2cs ex12 2021
+# SITES I USED: stackoverflow for GUI
+###################################
+
 from boggle_board_randomizer import LETTERS, randomize_board
-from pprint import pprint
-from time import time
-
-
 
 SIZE = 4
 PATHS = 1
@@ -11,11 +15,16 @@ WORDS = 2
 
 def is_valid_path(board, path, words):
     """
-
-    :param board:
-    :param path:
-    :param words:
-    :return:
+    checks if path is valid.
+    checks include:
+    1) if cell is already in path
+    2) if path is not continuous
+    3) cell is legal
+    4) word exists in dictionsary
+    :param board: list of lists (paths)
+    :param path: list of tuples
+    :param words: iterable of words
+    :return: string if path is valid. otherwise None
     """
     word = ""
     if path:
@@ -32,17 +41,13 @@ def is_valid_path(board, path, words):
             return word
 
 
-def board_to_dict(board):  # NOT IN USE
-    board_dict = {(i, j): board[i][j] for i in range(SIZE) for j in range(SIZE)}
-    return board_dict
-
-
 def is_legal_cell(cell, board):
     """
-
-    :param board:
-    :param cell:
-    :return:
+    checks if cell is legal.
+    meaning if coordinates exist on board.
+    :param board: list of lists
+    :param cell: tuple (coordinates)
+    :return:bool
     """
     max_index = len(board) - 1
     for i in cell:
@@ -52,6 +57,12 @@ def is_legal_cell(cell, board):
 
 
 def get_content(cell, board):
+    """
+    gets content of cell in board
+    :param cell: tuple of coordinates
+    :param board: list of lists, making a board
+    :return: string of cell content
+    """
     if is_legal_cell(cell, board):
         return board[cell[0]][cell[1]]
 
@@ -73,6 +84,13 @@ def is_neighbor(cell1, cell2):
 
 
 def all_neighbors(cell):
+    """
+    neighbors are all cells touching cell, including
+    diagonally. 8 in total.
+    Does not check the all neighbors exist!
+    :param cell: tuple
+    :return: list of tuples representing cell's neighbors
+    """
     moves = [-1, 0, 1]
     neighbors = [(cell[0] + i, cell[1] + j) for i in moves for j in moves]
     neighbors.remove(cell)
@@ -80,6 +98,21 @@ def all_neighbors(cell):
 
 
 def helper_1(n, cell, paths, board, words, func, path=None):
+    """
+    Helper function to find all of the paths.
+    Helps both find_length_n_paths and find_length_n_words.
+    :param n: length of path/words to find
+    :param cell: tuple
+    :param paths: list of all paths that we find
+    :param board: list of lists making a board
+    :param words: iterable object containing all legal words.
+    :param func: int representing which function is using the helper.
+    either finds words according to length of word, or according to length
+    of path.
+    :param path: path taken till this iteration of the function.
+    list of tuples.
+    :return: does not return anything. appends paths to existing list.
+    """
     if not path:
         path = []
     new_path = path[:] + [cell]
@@ -106,15 +139,26 @@ def helper_1(n, cell, paths, board, words, func, path=None):
 
 def path_to_word(path, board):
     """
-
-    :param path:
-    :param board:
-    :return:
+    gets string of letters on cells in path.
+    :param path: list of tuples.
+    :param board: list of lists making a board
+    :return: string
     """
     return "".join(board[cell[0]][cell[1]] for cell in path)
 
 
 def helper_2(n, board, words, func):
+    """
+    Uses helper 1 to find all paths of length n.
+
+    :param n: int
+    :param board: list of lists making a board
+    :param words: iterable containing strings
+    :param func:int representing which function is using the helper.
+    either finds words according to length of word, or according to length
+    of path.
+    :return: list of lists
+    """
     paths = []
     for i in range(SIZE):
         for j in range(SIZE):
@@ -129,21 +173,39 @@ def helper_2(n, board, words, func):
 
 
 def find_length_n_paths(n, board, words):
+    """
+    finds n length paths using helper 2
+
+    :return: list of lists of tuples (list of paths)
+    """
     return helper_2(n, board, words, PATHS)
 
 
 def find_length_n_words(n, board, words):
+    """
+    finds n length words using helper 2
+
+    :return: list of lists of tuples (list of paths)
+    """
     return helper_2(n, board, words, WORDS)
 
 
 def max_score_paths(board, words):
+    """
+    finds all paths of words on board. if there are multiple paths to the same
+    word, finds the path that gives the most points. if they gve the same
+    amount of points, keeps just one of them.
+    :param board: list of lists
+    :param words: iterable containing words.
+    :return: list of paths that give maximum score on given board.
+    """
     all_paths = {}
     longest = len(max(words, key=len))
     for i in range(1, longest):
         new_paths = {path_to_word(path, board): path for path in
                      find_length_n_paths(i, board, words)}
         for word in new_paths:
-            if word in all_paths:
+            if word in all_paths:  # finds path giving most points
                 if len(new_paths[word]) > len(all_paths[word]):
                     all_paths[word] = new_paths[word]
             else:
@@ -152,39 +214,29 @@ def max_score_paths(board, words):
 
 
 def readfile(file):
-    words = []
-
+    """
+    reads text file containing words, and puts them in a dictionary.
+    values are all 0 because they dont matter.
+    :param file: txt fild name
+    :return: dictionary of wirds
+    """
     f_words = open(file)
     lines = f_words.readlines()
     lines_dic = {line[:-1]: 0 for line in lines}
-    # for line in f_words:
-    #     word = line.strip()
-    #     if word.isalpha():
-    #         words.append(line.strip())
     f_words.close()
     return lines_dic
 
-
-if __name__ == '__main__':
-    bord = randomize_board(LETTERS)
-    bord1 = [['D', 'C', 'B', 'A'],
-             ['G', 'A', 'D', 'E'],
-             ['T', 'J', 'Y', 'T'],
-             ['N', 'M', 'F', 'I']]
-
-    milon = open("boggle_dict.txt")
-    lines = milon.readlines()
-    lines_dic = {line[:-1]: 0 for line in lines}
-    start = time()
-    all_paths = []
-    pprint(bord)
-    print(max_score_paths(bord, lines_dic))
-    # find_length_n_paths(10, bord1, lines_dic)
-    end = time()
-
-    print(end-start)
-
-
-
-
-
+# if __name__ == '__main__':
+#     bord = randomize_board(LETTERS)
+#     bord1 = [['D', 'C', 'B', 'A'],
+#              ['G', 'A', 'D', 'E'],
+#              ['T', 'J', 'Y', 'T'],
+#              ['N', 'M', 'F', 'I']]
+#
+#     milon = readfile("boggle_dict.txt")
+#     start = time()
+#     pprint(bord)
+#     print(len(max_score_paths(bord1, milon)))
+#     end = time()
+#
+#     print(end-start)
